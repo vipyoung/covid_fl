d1eb120f547b021f6dbbfd981ab7f9e67767b5ab
sfurini <furini.simone@gmail.com>
2020-12-03 12:20:40 +0100

    Update prepare_inputs.sh

--- prepare_inputs.sh
+++ prepare_inputs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # convert plink formats to text files
-#plink --bfile /path_to_M1/finalMask/MergeM1 --recode --tab --out /path_to_M1/finalMask/MergeM1
+plink --bfile /path_to_M1/finalMask/MergeM1 --recode --tab --out /path_to_M1/finalMask/MergeM1
 
 # to be sure that we all use the same list of features in the same order
 wget ftp://ftp.ensembl.org/pub/release-102/tsv/homo_sapiens/Homo_sapiens.GRCh38.102.refseq.tsv.gz
